af5f2c7d0944ec9d278efa0a61cadea055e45763
John Lovitsch <jmlovitsch@gmail.com>
2020-09-06 15:47:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,6 +15,27 @@ def introduction_with_language_optional(name, language = "Ruby")
    puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+require 'pry'
+a = {
+    :symbol => "4",
+    :kay => 65,
+    "gem" => "hattrick",
+    :yay => {
+            "cat" => "simon",
+            "dog" => "george",
+            "hamster" => "pickles",
+            }
+    }
+
+a.each do |key, value|
+  if value.to_s == value 
+    require.pry
+   puts value
+ else 
+   "I'm doing nothing"
+ end
+end
+
 
 
 
